b4188d0830aae925fee13a886314477c92963b69
dbhat <divyashri.bhat@gmail.com>
2014-03-31 11:16:01 -0400

    Install killall

--- Lab2_OpenFlow/postboot_debian/of-aggregator/of-topo-setup/gimibot/postboot_script_exo.sh
+++ Lab2_OpenFlow/postboot_debian/of-aggregator/of-topo-setup/gimibot/postboot_script_exo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 host=$1
 slice=`ruby -e "print '$2'[/[^+]*$/]"`
 echo $host > /etc/hostname
 /bin/hostname -F /etc/hostname
+apt-get install psmisc
 echo 'amqp://emmy9.casa.umass.edu' > /var/omf/communication_url
 echo $host-$slice > /var/omf/node_uri
 if grep 'Ubuntu' /etc/issue; then

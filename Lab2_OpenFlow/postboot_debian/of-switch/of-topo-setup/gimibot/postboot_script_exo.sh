7945c7c77120975d5384a9242862f91330988078
dbhat <divyashri.bhat@gmail.com>
2014-07-24 13:49:41 -0400

    Updating GIMI

--- Lab2_OpenFlow/postboot_debian/of-switch/of-topo-setup/gimibot/postboot_script_exo.sh
+++ Lab2_OpenFlow/postboot_debian/of-switch/of-topo-setup/gimibot/postboot_script_exo.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+export PATH=/usr/local/bin/rubydir/bin:$PATH
 host=$1
 slice=`ruby -e "print '$2'[/[^+]*$/]"`
 echo $host > /etc/hostname
 /bin/hostname -F /etc/hostname
-
 apt-get install psmisc
-
-echo 'amqp://emmy9.casa.umass.edu' > /var/omf/communication_url
-echo $host-$slice > /var/omf/node_uri
+echo "---
+:uid: $host-$slice
+:uri: amqp://gimi3.casa.umass.edu
+:environment: production
+:debug: false" > /etc/omf_rc/config.yml
+echo $PATH > /tmp/curpath.out
 if grep 'Ubuntu' /etc/issue; then
 restart omf_rc
 else
